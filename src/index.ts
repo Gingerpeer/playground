c2933396b183a3a58c418bee4403e83efab091bc
Gingerpeer PC <pierrevanzyl777@gmail.com>
2023-07-17 20:32:46 +0200

    kata to be done still

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-// import { encryptThis } from "./encryptThis";
+import { spinWords } from "./spinwords";
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+console.log(spinWords("Hey fellow warriors"))
---
+++ src/spinwords.ts
@@ -0,0 +1,8 @@
+export function spinWords(words: string): string {
+  /**
+   * Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata).
+   * Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+  //TODO Have fun :)
+  */
+  return words
+}
