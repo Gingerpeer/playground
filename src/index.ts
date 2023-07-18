dcd48fa9b0a8a1e7099d33d2dc256aad2df3d1fb
Gingerpeer PC <pierrevanzyl777@gmail.com>
2023-07-18 22:43:10 +0200

    variableStoreBetweenCalls not done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { spinWords } from "./spinwords";
-// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
-// spinWords( "This is a test") => returns "This is a test" 
-// spinWords( "This is another test" )=> returns "This is rehtona test"
-console.log(spinWords("This is another test"))
+import add from "./variableStoreBetweenCalls"
+
+let result = add(2)(10)
+
+console.log(result)
---
+++ src/variableStoreBetweenCalls.ts
@@ -0,0 +1,15 @@
+export default function add(n: number): any {
+  const f = function(x: number) {
+      return add(n + x);
+  };
+  
+  f.valueOf = function() {
+      return n;
+  };
+  
+  return f;
+}
+
+
+
+
